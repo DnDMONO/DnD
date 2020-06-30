2f44e72dbab8054c33eb6feaf70f8c192dcd4977
NikitaIkaev <61725946+NikitaIkaev@users.noreply.github.com>
2020-06-30 17:13:33 +0300

    Add files via upload

---
+++ AudioTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip SoundToPlay;
+    public float Volume;
+    AudioSource audio;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+}
+
